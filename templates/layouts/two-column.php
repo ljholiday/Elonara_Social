e33c1959b4bcfb6e4acebdd74bde17dcf1380bb3
Lonn Holiday <lonn@ljholiday.com>
2025-10-14 23:13:13 -0700

    Cleaned a comment in two page template

--- templates/layouts/two-column.php
+++ templates/layouts/two-column.php
@@ -129,19 +129,23 @@ $csrf_token = $security->createNonce('app_nonce', (int) $userId);
 <script src="<?= htmlspecialchars($assetBase . '/js/communities.js', ENT_QUOTES, 'UTF-8'); ?>"></script>
 <script src="<?= htmlspecialchars($assetBase . '/js/invitation.js', ENT_QUOTES, 'UTF-8'); ?>"></script>
 <?php endif; ?>
-//<script>
-//  fetch(<?= json_encode($assetBase . '/css/dev.css'); ?>, { method: 'HEAD' })
-//    .then(response => {
-//      if (response.ok) {
-//        const link = document.createElement('link');
-//        link.rel = 'stylesheet';
-//        link.href = '/assets/css/dev.css';
-//        document.head.appendChild(link);
-//      }
-//    })
-//    .catch(() => {
-//      // dev.css doesn't exist - silently ignore
-//    });
-//</script>
+    
+<!--
+<script>
+  fetch(<?= json_encode($assetBase . '/css/dev.css'); ?>, { method: 'HEAD' })
+    .then(response => {
+      if (response.ok) {
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = '/assets/css/dev.css';
+        document.head.appendChild(link);
+      }
+    })
+    .catch(() => {
+      // dev.css doesn't exist - silently ignore
+    });
+</script>
+-->
+
 </body>
 </html>
